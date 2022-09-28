fe87d7ddaa042d0faa6e5902065f8bd2d98a405c
hatchts <shatch86@gmail.com>
2022-09-27 18:14:57 -0600

    Created variables for first and last name

--- python_strings.py
+++ python_strings.py
@@ -2,13 +2,16 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Stacy'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Hatch'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_birth_year = 1969
 #   - current_year
 #       -set this equal to 2020
-
+current_year = 2022
 
 
 
